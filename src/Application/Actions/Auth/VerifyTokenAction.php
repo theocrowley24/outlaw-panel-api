c7b82029cadc599e2b6058ca4db8d3e2b971275a
Theo <theocrowley24@gmail.com>
2020-05-07 18:19:12 +0100

    Fixed login

--- src/Application/Actions/Auth/VerifyTokenAction.php
+++ src/Application/Actions/Auth/VerifyTokenAction.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace App\Application\Actions\Auth;
 
+use Slim\Psr7\Response as Psr7Response;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\RequestInterface as Request;
 
@@ -11,6 +12,13 @@ class VerifyTokenAction extends AuthAction
     protected function action(): Response
     {
         $authToken = $_COOKIE['authToken'];
+
+        if (!isset($_SESSION['token'])) {
+            $response = new Psr7Response();
+            $response->withStatus(401);
+            $response->getBody()->write(json_encode(array("message" => "Failed to verify. Please login.")));
+            return $response;
+        }
 
         return $this->respondWithData(array("verified" => $authToken === $_SESSION['token']));
     }
